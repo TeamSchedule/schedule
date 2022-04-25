0dd2b1c18313959cde2548039fd77ab699273c4a
Vsevolod <seva151201@gmail.com>
2022-04-25 12:01:33 +0700

    Remove sequence generator for User#id

--- src/main/java/com/schedule/team/model/entity/User.java
+++ src/main/java/com/schedule/team/model/entity/User.java
@@ -10,14 +10,5 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class User {
     @Id
-    @SequenceGenerator(
-            name = "user_sequence",
-            sequenceName = "user_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "user_sequence"
-    )
     private Long id;
 }
