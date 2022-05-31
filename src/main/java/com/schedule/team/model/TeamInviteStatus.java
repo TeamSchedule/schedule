0acfdb33cbd2daaf26d90060d6c46b5cf79a9c4d
Vsevolod <seva151201@gmail.com>
2022-05-31 12:06:14 +0700

    Add toLowerCase cast on TeamInviteStatus parsing

--- src/main/java/com/schedule/team/model/TeamInviteStatus.java
+++ src/main/java/com/schedule/team/model/TeamInviteStatus.java
@@ -14,15 +14,17 @@ public enum TeamInviteStatus {
     private final String code;
 
     TeamInviteStatus(String code) {
-        this.code=code;
+        this.code = code;
     }
 
     @JsonCreator
     public static TeamInviteStatus decode(final String code) {
         return Stream.of(
-                TeamInviteStatus.values()
-        ).filter(targetEnum -> targetEnum.code.equals(code))
-                .findFirst().orElse(null);
+                        TeamInviteStatus.values()
+                )
+                .filter(targetEnum -> targetEnum.code.equals(code.toLowerCase()))
+                .findFirst()
+                .orElse(null);
     }
 
     @JsonValue
