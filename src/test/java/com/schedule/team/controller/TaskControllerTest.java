3eabc1c0052dea50054569fd5f712833725a194d
Vsevolod <seva151201@gmail.com>
2022-06-01 13:11:07 +0700

    Add create task test

--- src/test/java/com/schedule/team/controller/TaskControllerTest.java
+++ src/test/java/com/schedule/team/controller/TaskControllerTest.java
@@ -6,6 +6,8 @@ import com.schedule.team.model.dto.TaskDTO;
 import com.schedule.team.model.entity.Task;
 import com.schedule.team.model.entity.Team;
 import com.schedule.team.model.entity.User;
+import com.schedule.team.model.request.CreateTaskRequest;
+import com.schedule.team.model.response.CreateTaskResponse;
 import com.schedule.team.repository.TaskRepository;
 import com.schedule.team.repository.TeamRepository;
 import com.schedule.team.repository.UserRepository;
@@ -20,8 +22,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -58,6 +60,48 @@ public class TaskControllerTest extends IntegrationTest {
     @AfterEach
     public void afterEach() {
         clearDb();
+    }
+
+    @Test
+    void createTaskTest() throws Exception {
+        User author = userRepository.save(new User(1L));
+        User assignee = userRepository.save(new User(2L));
+
+        Team team = teamRepository.save(new Team("test", LocalDate.now(), author));
+
+        String taskName = "test";
+        String taskDescription = "description";
+        LocalDateTime taskExpirationTime = LocalDateTime.of(12, 12, 12, 12, 12, 12);
+
+        CreateTaskRequest createTaskRequest = new CreateTaskRequest(
+                taskName,
+                taskDescription,
+                taskExpirationTime,
+                team.getId(),
+                assignee.getId()
+        );
+        String requestBody = objectMapper.writeValueAsString(createTaskRequest);
+
+        String response = mockMvc
+                .perform(
+                        post("/task/")
+                                .header(tokenHeaderName, tokenValue)
+                                .contentType(APPLICATION_JSON)
+                                .content(requestBody)
+                )
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        CreateTaskResponse createTaskResponse = objectMapper.readValue(response, CreateTaskResponse.class);
+        Task task = taskRepository.findById(createTaskResponse.getId()).get();
+        Assertions.assertNotNull(task);
+        Assertions.assertEquals(taskName, task.getName());
+        Assertions.assertEquals(taskDescription, task.getDescription());
+        Assertions.assertEquals(taskExpirationTime, task.getExpirationTime());
+        Assertions.assertEquals(team.getId(), task.getTeam().getId());
+        Assertions.assertEquals(assignee.getId(), task.getAssignee().getId());
     }
 
     @Test
